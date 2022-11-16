48e8bf9cf5ea847eb79ee5d9ef2c4e708242a70c
Daniel <danielgch@gmail.com>
2022-11-16 10:42:56 -0300

    Added SpringBoot project do manage products via Rest Webservice.

---
+++ productprice.server/src/test/java/awsjava/click/productprice/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package awsjava.click.productprice.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
